6debd21b2b60a4fe08e8171f47281d515120e413
niklas <niklas.koskela@gmail.com>
2025-07-10 18:08:32 +0300

    wip

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,10 +15,11 @@ module.exports = {
     es6: true,
   },
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
   },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '*.d.ts'],
 };
