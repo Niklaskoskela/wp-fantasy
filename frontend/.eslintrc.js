bfc2c6744ff6036c7a8e2aa814c598cd68bb457f
niklas <niklas.koskela@gmail.com>
2025-07-10 11:06:02 +0300

    ASD

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'react-app/jest',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
+
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
