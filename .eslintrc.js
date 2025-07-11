6df20061eecf64e3db0c08d015d04f9bcdb428e5
niklas <niklas.koskela@gmail.com>
2025-07-11 10:51:13 +0300

    Update brutality points

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./backend/tsconfig.json', './frontend/tsconfig.json', './shared/tsconfig.json'],
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '*.d.ts'],
+};
--- backend/src/config/points.ts
+++ backend/src/config/points.ts
@@ -23,7 +23,7 @@ export const pointsConfig = {
     contraFoul: -1,
     shot: -1,
     swimOff: 2,
-    brutality: -3,
+    brutality: -15,
     save: 1,
     win: 2
 }
